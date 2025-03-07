17c4b898c6697af9fe056133835568e041cf0bd9
SQUIDNOVA <squidnova04@gmail.com>
2025-03-07 22:54:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -14,3 +14,25 @@ document.onload = () => {
         document.querySelector('.header').classList.remove('active');
     }; I
 }
+
+
+// ======================================================================= Email code =============================================================================================
+
+
+function sendEmail() {
+    let params = {
+        name: document.getElementById("names").value,
+        email: document.getElementById("emailid").value,
+        message: document.getElementById("subjects").value,
+    }
+
+    emailjs.send('service_38n4v2r', 'template_9euo3dx', params)
+        .then(response => {
+            console.log("SUCCESS!", response.status, response.text);
+            alert("Message sent!");
+        })
+        .catch(error => {
+            console.error("FAILED...", error);
+            alert("Error sending email: " + error);
+        });
+}
